1bc7609e550bf5a16e3dcc3d6e6a4f1fdb05b19a
mnansary <silentassasin0111@gmail.com>
2022-05-10 03:36:17 +0600

    server-stream

--- scripts/script.sh
+++ scripts/script.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
-backs_dir="/home/ansary/WORK/Work/APSIS/datasets/SYNTH_INDIC/styles/"
-bn_fonts="/home/ansary/WORK/Work/APSIS/datasets/SYNTH_INDIC/fonts/bangla/"
-bn_std="/home/ansary/WORK/Work/APSIS/datasets/SYNTH_INDIC/fonts/bangla/Bangla.ttf"
-en_fonts="/home/ansary/WORK/Work/APSIS/datasets/SYNTH_INDIC/fonts/english/"
-en_std="/home/ansary/WORK/Work/APSIS/datasets/SYNTH_INDIC/fonts/english/English.ttf"
-save_dir="/home/ansary/WORK/Work/APSIS/datasets/SYNTH_INDIC/datasets/"
+backs_dir="/backup2/NID/data/styles/"
+bn_fonts="/backup2/NID/data/fonts/bangla/"
+bn_std="/backup2/NID/data/fonts/bangla/Bangla.ttf"
+en_fonts="/backup2/NID/data/fonts/english/"
+en_std="/backup2/NID/data/fonts/english/English.ttf"
+save_dir="/backup2/NID/data/datasets/"
 #-----------------------------------synthetic------------------------------------------
 python lang.py "bangla" "bn" $save_dir $bn_fonts $backs_dir $bn_std 
 python lang.py "english" "en" $save_dir $en_fonts $backs_dir $en_std --num_samples 20000
